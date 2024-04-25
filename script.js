04683a94bf006ddd23efde97ab4766376c7a0c96
Ana Cabral <141960250+AnaCabral07@users.noreply.github.com>
2024-04-25 20:13:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = prompt("Digite seu nome");
+let sobrenome = prompt("Digite seu sobrenome");
+ 
+alert('Seu nome completo é: ' + nome + " " + sobrenome);
+ 
+alert('Seu nome de catálogo é: ' + sobrenome.toUpperCase() + ", " + nome);
+
+//Dá certo essa merda
