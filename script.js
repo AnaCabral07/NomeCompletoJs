f6ed05c1999541f8797f081f620491b046ac2c79
Ana Clara <4n4.informatica@gmail.com>
2024-05-02 16:18:41 -0300

    Finaliza o programa 'NomeCompletoJS'

---
+++ script.js
@@ -0,0 +1,11 @@
+// Solicita o primeiro nome do usuário
+var nome = prompt("Olá usuário, poderia me informar seu primeiro nome?");
+
+// Solicita o sobrenome do usuário
+var sobrenome = prompt("Poderia me informar seu sobrenome?");
+
+// Exibe uma mensagem com o nome completo do usuário
+alert("Seu nome é: " + nome + " " + sobrenome);
+
+// Exibe uma mensagem com o sobrenome em letras maiúsculas, seguido pelo primeiro nome
+alert("Seu nome de catálogo: " + sobrenome.toUpperCase() + " , " + nome);
