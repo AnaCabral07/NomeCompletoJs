4a88c29b5cd69e4e342ad34f98de54762007d2fa
Ana Cabral <141960250+AnaCabral07@users.noreply.github.com>
2024-05-02 15:40:27 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let nome = prompt("Digite seu nome");
-let sobrenome = prompt("Digite seu sobrenome");
- 
-alert('Seu nome completo é: ' + nome + " " + sobrenome);
- 
-alert('Seu nome de catálogo é: ' + sobrenome.toUpperCase() + ", " + nome);
-
-//Dá certo essa merda
